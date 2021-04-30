111d32c00851c29e456555f6a3efe5d4e3804b5f
Swordmin <luciusmorir@gmail.com>
2021-04-30 20:45:23 +0300

    Fixed

--- Assets/Scripts/CubeLogic.cs
+++ Assets/Scripts/CubeLogic.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class CubeLogic : MonoBehaviour
 {
@@ -8,18 +7,28 @@ public class CubeLogic : MonoBehaviour
     [SerializeField] private bool bullet;
     [SerializeField] private float _jumpForce;
     [SerializeField] private GameObject cube;
-    public bool acitve;
+    [SerializeField] private int _id;
+    [SerializeField] private bool _inGame;
+    [SerializeField] private TextMeshProUGUI _textInfo;
     public void Start()
     {
+        _id = Random.Range(0,1000000);
         Init();
+        Invoke("SetGameState", 2);
         if (bullet)
             GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 25);
     }
 
     public int GetCount() => _count;
+    public int GetId() => _id;
     public void SetCount(int value) 
     {
         _count = value;
+        if (_count >= 2048)
+        {
+            Debug.Log("Win");
+            _textInfo.text = "Win";
+        }
         Init();
     }
     public void AddCount(int value) 
@@ -29,7 +38,7 @@ public class CubeLogic : MonoBehaviour
 
     public void Init() 
     {
-        if (_count < 2048)
+        if (_count <= 2048)
         {
             GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/" + _count.ToString());
         }
@@ -39,24 +48,40 @@ public class CubeLogic : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<CubeLogic>())
         {
-            if (int.Equals(collision.gameObject.GetComponent<CubeLogic>().GetCount(), GetCount()))
+            if (int.Equals(collision.gameObject.GetComponent<CubeLogic>().GetCount(), GetCount()) && GetId() > collision.gameObject.GetComponent<CubeLogic>().GetId())
             {
                 Destroy(gameObject);
                 Destroy(collision.gameObject);
-                if (acitve)
-                {
-                    GameObject CubePref = Instantiate(cube, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.identity);
-                    CubePref.GetComponent<CubeLogic>().Jump();
-                    CubePref.GetComponent<CubeLogic>().SetCount(collision.gameObject.GetComponent<CubeLogic>().GetCount() + GetCount());
-                    CubePref.GetComponent<CubeLogic>().Init(); 
-                }
+                GameObject CubePref = Instantiate(cube, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.identity);
+                CubePref.GetComponent<CubeLogic>().Jump();
+                CubePref.GetComponent<CubeLogic>().SetCount(collision.gameObject.GetComponent<CubeLogic>().GetCount() + GetCount());
+                CubePref.GetComponent<CubeLogic>().Init();
 
             }
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Finish")
+        {
+            if (_inGame)
+            {
+                _textInfo.text = "Fail";
+            }
+        }
+    }
+
     private void Jump()
-    { 
-        GetComponent<Rigidbody>().AddForce(new Vector3(0, _jumpForce, 0), ForceMode.Impulse);
+    {
+        float x = Random.Range(-1f,1f);
+        float z = Random.Range(-1f, 1f);
+        GetComponent<Rigidbody>().AddForce(new Vector3(x * _jumpForce, _jumpForce, z * _jumpForce), ForceMode.Impulse);
+    }
+
+
+    public void SetGameState() 
+    {
+        _inGame = true;
     }
 }
